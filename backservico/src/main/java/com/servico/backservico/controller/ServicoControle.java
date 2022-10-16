759a41a88a164687c58384a7363e8a188fed91a7
JoaoFelipe25 <joaofelipemarcelinodacruz@yahoo.com.br>
2022-10-16 20:55:15 -0300

    sistema de servico

--- backservico/src/main/java/com/servico/backservico/controller/ServicoControle.java
+++ backservico/src/main/java/com/servico/backservico/controller/ServicoControle.java
@@ -2,10 +2,9 @@ package com.servico.backservico.controller;
 
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,43 +20,49 @@ import com.servico.backservico.service.ServicoService;
 @RestController
 @RequestMapping("/api/servico")
 public class ServicoControle {
-    
 
     @Autowired
     private ServicoService servicoService;
 
     @GetMapping("/")
+    @CrossOrigin("http://localhost:3000")
     public List<Servico>buscarTodos(){
         return servicoService.buscarTodos();
     }
 
     @GetMapping("/pagamentoPendente")
+    @CrossOrigin("http://localhost:3000")
     public List<Servico> buscarServicosPagamentoPendente(){
         return servicoService.buscarServicosPagamentoPendente();
     }
 
     @GetMapping("/cancelado")
+    @CrossOrigin("http://localhost:3000")
     public List<Servico>buscarServicosCancelados(){
         return servicoService.buscarServicosCancelados();
     }
 
     @PostMapping("/")
+    @CrossOrigin("http://localhost:3000")
     public Servico inserir(@RequestBody Servico servico){
         return servicoService.inserir(servico);
     }
 
     @PostMapping("/{id}")
+    @CrossOrigin("http://localhost:3000")
     public ResponseEntity<Void> cancelar(@PathVariable("id") long id){
         servicoService.cancelarServico(id);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("/")
+    @CrossOrigin("http://localhost:3000")
     public Servico alterar(@RequestBody Servico servico){
         return servicoService.alterar(servico);
     }
 
     @DeleteMapping("/{id}")
+    @CrossOrigin("http://localhost:3000")
     public ResponseEntity<Void> excluir(@PathVariable("id") Long id){
         servicoService.excluir(id);
         return ResponseEntity.ok().build(); 
